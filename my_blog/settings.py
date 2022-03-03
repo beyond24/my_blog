22326bed2e1a04972457f6696d21c2259f7e69e8
beyond24 <beyond_24@qq.com>
2022-03-03 20:00:41 +0800

    title

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'sql_file/db.sqlite3',
     }
 }
 
